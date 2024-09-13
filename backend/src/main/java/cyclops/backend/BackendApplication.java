d38a4e4967dc1af393faff999a1e5e937b440fbe
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2024-09-13 10:00:56 -0300

    Backend

---
+++ backend/src/main/java/cyclops/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package cyclops.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
