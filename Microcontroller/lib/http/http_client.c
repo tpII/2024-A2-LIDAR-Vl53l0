874f95b35ae247a0f2d328d78f7ee720b0efbc28
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2024-09-25 00:27:52 -0300

    Http handler fix

--- Microcontroller/lib/http/http_client.c
+++ Microcontroller/lib/http/http_client.c
@@ -1,5 +1,6 @@
 #include "http_client.h"
 #include "esp_log.h"
+#include "esp_err.h"
 #include "esp_http_client.h"
 #include "cJSON.h"
 #include <string.h>
--- Microcontroller/lib/http/http_handler.h
+++ Microcontroller/lib/http/http_handler.h
@@ -7,6 +7,6 @@ esp_err_t getInstruccionMessage(char *msg);
 
 esp_err_t sendControlMessage(const char *msg, const size_t length);
 
-esp_err_t sendSensorValue(const char *msg, const unit8_t *distance,const unit8_t *angle);
+esp_err_t sendSensorValue(const uint16_t distance, const uint16_t angle);
 
 #endif //HTTP_HANDLER_H_
