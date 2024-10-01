dfc59baac7a323f613450aa47348726a0eb8c976
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2024-10-01 00:02:09 -0300

    Test program

--- Microcontroller/src/main.c
+++ Microcontroller/src/main.c
@@ -1,9 +1,16 @@
-#include "http_client.h"
-#include "i2c.h"
-#include "lights.h"
+#include "ap_server.h"
 
-
+static const char *TAG = "MAIN";
 
 void app_main(void) {
+
+    esp_err_t err =  wifi_init_softap();
+    if(err){
+        ESP_LOGE(TAG, "Error ocurred trying to establish wifi AP: err %d", err);
+    }
    
+   esp_err_t err2 = initialize_server();
+   if(err2){
+     ESP_LOGE(TAG, "Error ocurred trying to initialize server: err %d", err);
+   }
 }
