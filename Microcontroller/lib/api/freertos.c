6f44ec04061c8587cd6e98653e40d7da74d8c5bb
florencia-ossola <90108766+florencia-ossola@users.noreply.github.com>
2024-10-13 20:47:16 -0300

    agrego para inicializar servidor http

--- Microcontroller/lib/api/freertos.c
+++ Microcontroller/lib/api/freertos.c
@@ -3,10 +3,16 @@
 static const char *TAG = "FreeRTOS";
 
 static void access_point(void *pvParameters);
+static void http_server(void *pvParameters);
 
 esp_err_t initialize_access_point(void)
 {
     xTaskCreatePinnedToCore(access_point, "AP Server", 4096, NULL, 1, NULL, 0);
+    return ESP_OK;
+}
+
+esp_err_t initialize_http_server(void){
+    xTaskCreatePinnedToCore(http_server, "HTTP Server", 4096, NULL, 1, NULL, 0);
     return ESP_OK;
 }
 
@@ -15,5 +21,13 @@ static void access_point(void *pvParameters)
     esp_err_t err = initialize_server();
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "Failed to initialize AP server: err %d", err);
+    }
+}
+
+static void http_server(void *pvParameters)
+{
+    esp_err_t err = start_http_server();
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize HTTP server: err %d", err);
     }
 }
--- Microcontroller/lib/api/freertos.h
+++ Microcontroller/lib/api/freertos.h
@@ -4,5 +4,9 @@
 #include <esp_err.h>
 #include <esp_log.h>
 #include "ap_server.h"
+#include "http_server.h"
+
+esp_err_t initialize_access_point(void);
+esp_err_t initialize_http_server(void);
 
 #endif
