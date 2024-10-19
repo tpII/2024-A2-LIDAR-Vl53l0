7978c1aee085fba3c053aaef861e4831c300e040
florencia-ossola <90108766+florencia-ossola@users.noreply.github.com>
2024-10-19 13:44:28 -0300

    agrego funcion para crear/abrir archivo y que agregue la fecha actual

--- Microcontroller/lib/api/message_log.c
+++ Microcontroller/lib/api/message_log.c
@@ -1,6 +1,7 @@
 #include "message_log.h"
 
 static char * get_message(void);
+static char * get_date(void);
 
 static char * get_message(void)
 {
@@ -17,7 +18,37 @@ void get_report_log(void)
     //
 }
 
-void create_report_log(void)
+FILE * open_report_log(void)
 {
-    //
+    FILE * file;
+    file = fopen("data/report-log.txt", "a");
+    
+    if(file == NULL)
+    {
+        printf("Unable to create or open file.\n");
+    }
+    
+    char *date = get_date();
+    if (date != NULL) {
+        fputs(date, file);  
+    }
+
+    return file;
+}
+
+static char * get_date(void)
+{
+    time_t t;
+    struct tm *tm_info;
+
+    time(&t);
+    tm_info = localtime(&t);
+
+    char buffer[11]; 
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d", tm_info);
+
+    char date[50]; 
+    snprintf(date, sizeof(date), "Date: %s", buffer);
+
+    return date;
 }
--- Microcontroller/lib/api/message_log.h
+++ Microcontroller/lib/api/message_log.h
@@ -1,6 +1,12 @@
 #ifndef _MESSAGE_LOG_H_
 #define _MESSAGE_LOG_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DATA_SIZE 1000  // revisar
+
 void write_message(char *msg);
 void get_report_log(void);
 void create_report_log(void);
