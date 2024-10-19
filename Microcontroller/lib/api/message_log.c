6fc54c3bc96486a209739fbd4834c1f6e91d43c6
florencia-ossola <90108766+florencia-ossola@users.noreply.github.com>
2024-10-19 13:17:39 -0300

    creo libreria para crear un report log (solo estructura)

---
+++ Microcontroller/lib/api/message_log.c
@@ -0,0 +1,23 @@
+#include "message_log.h"
+
+static char * get_message(void);
+
+static char * get_message(void)
+{
+    // pass
+}
+
+void write_message(char *msg)
+{
+    //
+}
+
+void get_report_log(void)
+{
+    //
+}
+
+void create_report_log(void)
+{
+    //
+}
---
+++ Microcontroller/lib/api/message_log.h
@@ -0,0 +1,8 @@
+#ifndef _MESSAGE_LOG_H_
+#define _MESSAGE_LOG_H_
+
+void write_message(char *msg);
+void get_report_log(void);
+void create_report_log(void);
+
+#endif 
