41d74e89cac71b01ed41afd02b55a5a035029cf7
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2024-11-02 17:31:01 -0300

    The basic characteristics are established

--- Frontend/src/app/views/main/main.component.ts
+++ Frontend/src/app/views/main/main.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { AngularSplitModule } from 'angular-split';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [MatSidenavModule, MatIconModule, AngularSplitModule, CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
+  // Controlar si la sidebar está expandida o no
+  isSideNavExpanded = false;
 
+  // Tamaños mínimo y máximo de la sidebar
+  minSizeSidenav: number = 7; // Porcentaje para la sidebar colapsada
+  maxSizeSidenav: number = 26; // Porcentaje para la sidebar expandida
+
+  // Alternar el estado de la sidebar entre colapsada y expandida
+  toggleSidebar(): void {
+    this.isSideNavExpanded = !this.isSideNavExpanded;
+    console.log('Sidebar expanded:', this.isSideNavExpanded);
+  }
 }
