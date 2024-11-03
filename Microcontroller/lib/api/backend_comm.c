2b597b6d5b3c8579602cc1a342d98edee80a908c
florencia-ossola <90108766+florencia-ossola@users.noreply.github.com>
2024-11-03 18:22:15 -0300

    agrego tags en el envio de mensajes al backend

--- Microcontroller/lib/api/backend_comm.c
+++ Microcontroller/lib/api/backend_comm.c
@@ -1,6 +1,7 @@
 #include "backend_comm.h"
 
 static const char *TAG = "Backend Communication";
+static const char * get_topic(const char *ESP_TAG);
 
 esp_err_t backend_init() {
 
@@ -14,20 +15,61 @@ esp_err_t backend_init() {
     return ESP_OK;
 }
 
-esp_err_t send_message_to_backend(const char *message) {
+esp_err_t send_message_to_backkend(const char* ESP_TAG, const char *msg) {
     const char *topic = "/backend/esp32-msg";
-    char payload[256];
-    snprintf(payload, sizeof(payload), "%s", message);
+    /* segun el tag enviar a diferents topics
+    const char *topic = get_topic(ESP_TAG); 
+    if (topic == NULL) {
+        ESP_LOGE(TAG, "ESP_TAG received unrecognized.");
+        ENVIAR AL REPORT LOG
+        return ESP_FAIL;
+    }
+    */
+    const char *values[2];
+    const char *keys[2] = {"tag", "message"};
+    char esp_tag[256];
+    char esp_msg[256];
+    snprintf(esp_tag, sizeof(esp_tag), "%s", ESP_TAG);
+    snprintf(esp_msg, sizeof(esp_msg), "%s", msg);
+    values[0] = esp_tag;
+    values[1] = esp_msg;
+    cJSON *json = NULL;
 
-    esp_err_t err = mqtt_publish(topic, message);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to publish a message.");
+    esp_err_t err = create_json_data(&json, keys, values, 2);
+    print_json_data(json);
+
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Error creating json data: %s", esp_err_to_name(err));
+        // ERROR A REPORT LOG
+        return err;
+    }
+
+    esp_err_t err2 = mqtt_publish(topic, json);
+    if (err2 != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to publish a message.",  esp_err_to_name(err));
         // ERROR AL REPORT LOG
         return ESP_FAIL;
     }
 
     return ESP_OK;
+}
 
+static const char * get_topic(const char *ESP_TAG) {
+    if (ESP_TAG == "ERROR") {
+       return "/backend/esp32-msg/error";
+    }
+    if (ESP_TAG == "WARNING") {
+       return "/backend/esp32-msg/warning";
+    }
+    if (ESP_TAG == "SUCCESS") {
+       return "/backend/esp32-msg/success";
+    }
+    if (ESP_TAG == "INFO") {
+       return "/backend/esp32-msg/info";
+    }
+    
+    return NULL;
 }
 
 esp_err_t backend_disconnect() {
--- Microcontroller/lib/api/backend_comm.h
+++ Microcontroller/lib/api/backend_comm.h
@@ -4,12 +4,13 @@
 #include <stdio.h>
 #include <esp_err.h>
 //#include "mqtt_server.h"
+#include <cJSON.h>
 
 // Inicialización de conexión al backend
 esp_err_t backend_init(void);
 
 // Enviar mensaje al backend
-esp_err_t send_to_backend(const char *topic, const char *message);
+esp_err_t send_message_to_backkend(const char* ESP_TAG, const char *msg);
 
 // Finalizar la conexión
 esp_err_t backend_disconnect();
