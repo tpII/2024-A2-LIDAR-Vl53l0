05a4de2d777c8a3166f80494b54193c3ca03f509
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2024-11-24 10:21:19 -0300

    x

--- Frontend/src/app/views/main/main.component.ts
+++ Frontend/src/app/views/main/main.component.ts
@@ -14,10 +14,15 @@ import { CommonModule } from '@angular/common';
 export class MainComponent {
   // Controlar si la sidebar está expandida o no
   isSideNavExpanded = false;
+ // Tamaños mínimo y máximo de la sidebar
+ minSizeSidenav: number = 7; // Porcentaje para la sidebar colapsada
+ maxSizeSidenav: number = 26; // Porcentaje para la sidebar expandida
 
-  // Tamaños mínimo y máximo de la sidebar
-  minSizeSidenav: number = 7; // Porcentaje para la sidebar colapsada
-  maxSizeSidenav: number = 26; // Porcentaje para la sidebar expandida
+  // Agregar propiedades para el mando y la batería
+  isControllerConnected = true; // Cambiar dinámicamente según el estado real
+  batteryLevel = 75; // Cambiar dinámicamente según el nivel de batería
+
+ 
 
   buttons = [
     { icon: 'speed', label: '20m x hora' },
@@ -26,7 +31,7 @@ export class MainComponent {
     { icon: 'restart_alt', label: 'Reiniciar' },
     { icon: 'cancel', label: 'Restablecer' },
   ];
-  
+
   // Alternar el estado de la sidebar entre colapsada y expandida
   toggleSidebar(): void {
     this.isSideNavExpanded = !this.isSideNavExpanded;
