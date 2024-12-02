623a44d484882ebc1f4414af199154ac2697327a
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2024-12-02 19:35:02 -0300

    Testing Lidar+servo

--- Microcontroller/src/main.c
+++ Microcontroller/src/main.c
@@ -1,40 +1,36 @@
 #include "esp_log.h"
-//#include "ap_server.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-//#include "mqtt_server.h"
+#include "mapping.h"
 
-//#include "mqtt_handler.h"
-
-#include "vl53l0x.h"
-#include "i2c.h"
-#include <stdio.h>
-#include <string.h>
-#include "ina219.h"
 static const char *TAG = "MAIN";
 
-
 #define VL53L0X
-//#define VL61L0X
+// #define VL61L0X
 
-
-
-int app_main(void){   
+int app_main(void)
+{
         esp_err_t err;
 
-        ESP_LOGI(TAG, "Iniciando GPIO...");
-        err = gpio_init();
-        if(err != ESP_OK){
-                ESP_LOGE(TAG, "Error en el gpio_init: %s", esp_err_to_name(err));
+        ESP_LOGI(TAG, "Iniciando Mapping Service...");
+        err = mapping_init();
+        if (err != ESP_OK)
+        {
+                ESP_LOGE(TAG, "Error Iniciando Mapping:  %s", esp_err_to_name(err));
                 return 1;
         }
 
-        ESP_LOGI(TAG, "Iniciando I2C...");
-        err = i2c_init();
-        if(err != ESP_OK){
-                ESP_LOGE(TAG, "Error en el i2c_master_init: %s", esp_err_to_name(err));
-                return 2;
+        uint16_t angle = 0;
+        uint16_t distance = 0;
+        while (1)
+        {
+                err = getMappingValue(&angle,&distance);
+                if(err != ESP_OK){
+                        ESP_LOGE(TAG,"FAIL TO GET MAPPING VALUE");
+                }
+                ESP_LOGW(TAG,"Mapping Values: %d° - %dmm",angle,distance);
+                vTaskDelay(200);
         }
 
-       return 0;
+        return 0;
 }
