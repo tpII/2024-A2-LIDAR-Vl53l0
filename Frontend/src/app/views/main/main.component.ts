05860a4b6c2c8f72ec460f4ecc840b5d0e92867e
florencia-ossola <90108766+florencia-ossola@users.noreply.github.com>
2024-12-04 19:33:46 -0300

    Modifico boton de velocidad

--- Frontend/src/app/views/main/main.component.ts
+++ Frontend/src/app/views/main/main.component.ts
@@ -4,11 +4,12 @@ import { MatIconModule } from '@angular/material/icon';
 import { AngularSplitModule } from 'angular-split';
 import { CommonModule } from '@angular/common';
 import { GamepadComponent } from '../../shared/components/gamepad/gamepad.component';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [MatSidenavModule, MatIconModule, AngularSplitModule, CommonModule, GamepadComponent],
+  imports: [MatSidenavModule, MatIconModule, AngularSplitModule, CommonModule, GamepadComponent, MatMenuModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
@@ -23,15 +24,27 @@ export class MainComponent {
   isControllerConnected = true; // Cambiar dinámicamente según el estado real
   batteryLevel = 75; // Cambiar dinámicamente según el nivel de batería
 
- 
+ //speedButton = { icon: 'speed', label: '20m x hora' }
+ //speeds = ["Lento", "Normal", "Rápido"]; // Opciones de velocidad
 
   buttons = [
-    { icon: 'speed', label: '20m x hora' },
+    { icon: 'speed', label: 'Normal' },
     { icon: 'thermostat', label: 'Máximo 30°' },
     { icon: 'pause', label: 'PAUSAR' },
     { icon: 'restart_alt', label: 'REINICIAR' },
     { icon: 'cancel', label: 'RESTABLECER' },
   ];
+
+
+    toggleSpeedMenu(event: MouseEvent) {
+      event.stopPropagation();  // Esto previene que el clic se propague al sidebar
+      // Aquí va tu lógica para mostrar el menú de velocidad
+    }
+  
+
+  onSpeedClick(speed: string) {
+    console.log('Velocidad seleccionada:', speed);
+  }
 
   // Alternar el estado de la sidebar entre colapsada y expandida
   toggleSidebar(): void {
@@ -63,15 +76,25 @@ export class MainComponent {
     event.stopPropagation();
     console.log('Botón presionado:', button.label);
   
-    if (button.label === 'PAUSAR') {
+    switch (button.label) {
+      case 'PAUSAR':
         button.icon = 'play_arrow';
-        button.label = 'REANUDAR';  // Cambiar el texto
-      }
-    else {
-        if (button.label === 'REANUDAR') {
-          button.icon = 'pause';
-          button.label = 'PAUSAR';  // Cambiar el texto
-        }
+        button.label = 'REANUDAR';
+        break;
+      case 'REANUDAR':
+        button.icon = 'pause';
+        button.label = 'PAUSAR';
+        break;
+      case 'Lento':
+        button.label = 'Normal';
+        break;
+      case 'Normal':
+        button.label = 'Rápido';
+        break;
+      case 'Rápido':
+        button.label = 'Lento';
+        break;
+      // Añadir más casos para otros botones si es necesario
     }
   
   }
