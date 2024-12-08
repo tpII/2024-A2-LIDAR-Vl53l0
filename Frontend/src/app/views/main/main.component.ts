febf71d6a4b7adaa55ec77bb028012f19de98485
florencia-ossola <90108766+florencia-ossola@users.noreply.github.com>
2024-12-07 22:49:08 -0300

    enviar instruccion

--- Frontend/src/app/views/main/main.component.ts
+++ Frontend/src/app/views/main/main.component.ts
@@ -5,6 +5,7 @@ import { AngularSplitModule } from 'angular-split';
 import { CommonModule } from '@angular/common';
 import { GamepadComponent } from '../../shared/components/gamepad/gamepad.component';
 import { MatMenuModule } from '@angular/material/menu';
+import { InstructionsService } from '../../core/services/instructions.service';
 
 @Component({
   selector: 'app-main',
@@ -29,12 +30,13 @@ export class MainComponent {
 
   buttons = [
     { icon: 'speed', label: 'Normal' },
-    { icon: 'save', label: 'Guardar Mapeo' },
+    { icon: 'download', label: 'Guardar Mapeo' },
     { icon: 'pause', label: 'PAUSAR' },
     { icon: 'restart_alt', label: 'REINICIAR' },
     { icon: 'cancel', label: 'RESTABLECER' },
   ];
 
+    constructor(private InstructionsService: InstructionsService) {}
 
     toggleSpeedMenu(event: MouseEvent) {
       event.stopPropagation();  // Esto previene que el clic se propague al sidebar
@@ -75,7 +77,7 @@ export class MainComponent {
     // Detiene la propagación del evento para evitar que afecte al sidenav
     event.stopPropagation();
     console.log('Botón presionado:', button.label);
-  
+
     switch (button.label) {
       case 'PAUSAR':
         button.icon = 'play_arrow';
@@ -87,18 +89,35 @@ export class MainComponent {
         break;
       case 'Lento':
         button.label = 'Normal';
+        this.sendInstruction("MEDIUM")
         break;
       case 'Normal':
         button.label = 'Rápido';
+        this.sendInstruction("FAST")
         break;
       case 'Rápido':
         button.label = 'Lento';
+        this.sendInstruction("SLOW")
         break;
-        case 'Guardar Mapeo':
-          // Funcion para sacar captura al mapa
-          console.log("Guardar mapeo")
-          break;
+      case 'Guardar Mapeo':
+        // Funcion para sacar captura al mapa
+        console.log("Guardar mapeo")
+        break;
     }
+  
+  }
+
+  sendInstruction(speed: string): void {
+    const instruction = { speed: speed };
+
+    this.InstructionsService.createInstruction(instruction).subscribe({
+      next: (response) => {
+        console.log('Instrucción creada:', response);
+      },
+      error: (error) => {
+        console.error('Error al crear la instrucción:', error);
+      }
+    });
   
   }
   
