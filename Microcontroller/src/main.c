dab61729f300f91e4e8dd57fa3ca94621ad41e30
leonelg99 <lguerrico@alu.ing.unlp.edu.ar>
2025-01-23 00:27:08 -0300

    All mech/electric functions tested

--- Microcontroller/src/main.c
+++ Microcontroller/src/main.c
@@ -3,6 +3,8 @@
 #include "freertos/task.h"
 #include "mapping.h"
 #include "battery.h"
+#include "freeRtosTasks.h"
+#include "motors.h"
 
 static const char *TAG = "MAIN";
 
@@ -31,11 +33,31 @@ int app_main(void)
         }
         ESP_LOGI(TAG, "Mapping Service Iniciado!");
 
+        ESP_LOGI(TAG, "Iniciando Tareas...");
+        err = createTasks();
+        if (err != ESP_OK)
+        {
+                ESP_LOGE(TAG, "Error Iniciando Tareas:  %s", esp_err_to_name(err));
+                return 1;
+        }
+        ESP_LOGI(TAG, "Tareas Iniciadas!");
+
+        ESP_LOGI(TAG, "Iniciando Motores Service...");
+        motors_setup();
+        ESP_LOGI(TAG, "Motores Service Iniciado!");
+
         uint16_t angle = 0;
         uint16_t distance = 0;
         uint8_t battery_level = 0;
         while (1)
-        {
+        {       
+                ESP_LOGW(TAG, "Going FORWARD");
+                motors_command(FORWARD);
+                vTaskDelay(200);
+                ESP_LOGW(TAG, "Going BACKWARD");
+                motors_command(BACKWARD);
+                vTaskDelay(200);
+
                 err = getMappingValue(&angle,&distance);
                 if(err != ESP_OK){
                         ESP_LOGE(TAG,"FAIL TO GET MAPPING VALUE");
